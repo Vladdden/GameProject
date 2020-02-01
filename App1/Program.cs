7960e0780adb4d58e9aec793db95a92667a16f01
Vladdden <Vlad2999009@yandex.ru>
2020-02-01 16:31:13 +0300

    notebook test

--- App1/Program.cs
+++ App1/Program.cs
@@ -33,7 +33,8 @@ namespace Game
 
         static void Main()
         {
-            Tavern();
+			labyrinth ();
+			// kfkfkk
             n = 0;
             Console.WriteLine("\t\t\t\tMAGIC WAR\n\n\n");
             Console.WriteLine("Предыстория: Еще вчера все было хорошо и везде царил мир и покой. \nНо тут случилось то, чего не ожидал никто." +
